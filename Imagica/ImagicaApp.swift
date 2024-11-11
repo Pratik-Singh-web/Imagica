712f214a3dcbda85f301000fd15779d80b8cda59
Pratik Kumar Singh <pratik.singh1@indiamart.com>
2024-11-11 18:05:44 +0530

    Initial Commit

---
+++ Imagica/ImagicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagicaApp.swift
+//  Imagica
+//
+//  Created by Pratik Kumar Singh on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ImagicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
